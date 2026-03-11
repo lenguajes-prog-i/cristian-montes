362bb30e9bb1f3b5e921b4aaa97931da372d4fe4
CristianMontes94 <camilomontes11@gmail.com>
2026-03-10 20:46:28 -0500

    validacion de credenciales

---
+++ main.py
@@ -0,0 +1,9 @@
+user = "admin@uniremington.edu.co"
+password = "admin123"
+
+user_input = input("Ingrese su usuario: ")
+password_input = input("Ingrese su contraseña: ")   
+if user_input == user and password_input == password:
+    print("¡Bienvenido!")   
+else:    print("Usuario o contraseña incorrectos. Inténtelo de nuevo.") 
+
